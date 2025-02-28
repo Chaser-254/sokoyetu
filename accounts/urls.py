87f14b293f47f1127482de544e15ce0aadc22076
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-02-28 15:45:15 +0300

    added password reset capabilities

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
+    path('password_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+    path('password_change_done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password_reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password_reset/complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    
 ]
