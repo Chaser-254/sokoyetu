97e41ee5886613dbc3da4d514b75e8aea553755f
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-02-28 23:31:13 +0300

    improved the user interface and user experience

--- listings/forms.py
+++ listings/forms.py
@@ -1,20 +1,17 @@
 from django import forms
 from .models import Review
 
-REVIEW_CHOICES = [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')]
-
 class ReviewForm(forms.ModelForm):
+    RATING_CHOICES = [(i, f'{i} ⭐') for i in range(1, 6)]  # Adds star icons next to numbers
+
+    rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.RadioSelect(attrs={'class': 'rating-stars'})
+    )
+
     class Meta:
         model = Review
         fields = ['text', 'rating']
         widgets = {
-            'text': forms.Textarea(
-                attrs={
-                    'class': 'form-control shadow px-2',
-                    'rows': 6
-               }
-            ),
-            'rating': forms.RadioSelect(
-                choices=REVIEW_CHOICES
-             )
+            'text': forms.Textarea(attrs={'class': 'form-control shadow px-2', 'rows': 6})
         }
--- listings/views.py
+++ listings/views.py
@@ -21,9 +21,11 @@ def product_list(request, category_slug=None):
             'products': products,
         }
     )
-
 def product_detail(request, category_slug, product_slug):
     product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
+    
+    
+    related_products = Product.objects.filter(category=product.category).exclude(id=product.id)[:4]
     
     if request.method == 'POST':
         review_form = ReviewForm(request.POST)
@@ -31,12 +33,23 @@ def product_detail(request, category_slug, product_slug):
         if review_form.is_valid():
             cf = review_form.cleaned_data
             author_name = "Anonymous"
-            if request.user.is_authenticated and request.user.first_name != '':
+            if request.user.is_authenticated and request.user.first_name:
                 author_name = request.user.first_name
-            Review.objects.create(product=product, author=author_name, rating=cf['rating'], text=cf['text'])
+            
+            Review.objects.create(
+                product=product, 
+                author=author_name, 
+                rating=cf['rating'], 
+                text=cf['text']
+            )
             return redirect('listings:product_detail', category_slug=category_slug, product_slug=product_slug)
     else:
         review_form = ReviewForm()
         cart_product_form = CartAddProductForm()
 
-    return render(request, 'product/detail.html', {'product': product,'review_form': review_form,'cart_product_form': cart_product_form})
+    return render(request, 'product/detail.html', {
+        'product': product,
+        'review_form': review_form,
+        'cart_product_form': cart_product_form,
+        'related_products': related_products
+    })
