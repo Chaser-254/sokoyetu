32da3003f2f506c55e83ddfecf8cd62d34570803
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-02-28 14:03:36 +0300

    added user registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,31 @@
 from django import forms
+from django.contrib.auth.models import User
+
+class UserRegistrationForm(forms.ModelForm):
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label="Confirm Password"
+    )
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'password')  # Remove 'password2' from here
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get("password2")
+
+        if password and password2 and password != password2:
+            self.add_error('password2', "Passwords do not match.")
+
 
 class LoginForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib import messages
+
+def register(request):
+    user_form = UserRegistrationForm()
+
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            cf = user_form.cleaned_data
+
+            email = cf['email']
+            password = cf['password']
+            password2 = cf['password2']
+
+            if User.objects.filter(email=email).exists():
+                messages.error(request, 'User with that email account already exists')
+            elif password != password2:
+                messages.error(request, 'Passwords don\'t match')
+            else:
+                new_user = User.objects.create_user(
+                    first_name=cf['first_name'],
+                    last_name=cf['last_name'],
+                    username=email,
+                    password=password
+                )
+                return render(request, 'accounts/register_done.html', {'new_user': new_user})
+
+    return render(request, 'accounts/register.html', {'user_form': user_form})
+
 
 def user_login(request):
     form = LoginForm(request.POST or None)
 
     if request.method == 'POST':
         if form.is_valid():
-            cf = form.cleaned_data  # Use `cf = form.cleaned_data`, not `cf = form.cleaned_data()`
+            cf = form.cleaned_data  
             try:
                 user_record = User.objects.get(email=cf['email'])
             except User.DoesNotExist:
@@ -23,5 +51,4 @@ def user_login(request):
                     return redirect('listings:product_list')
                 messages.error(request, 'Incorrect email / password')
 
-    # Always return a response
     return render(request, 'accounts/login.html', {'form': form})
