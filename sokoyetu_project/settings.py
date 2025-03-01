f880bdd69c3bb462a46c74bbda2579e6f91d699f
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-03-01 22:31:33 +0300

    minor changes

--- sokoyetu_project/settings.py
+++ sokoyetu_project/settings.py
@@ -79,7 +79,7 @@ USE_TZ = True
 
 # Static files settings
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sokoyetu_project/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files handling
