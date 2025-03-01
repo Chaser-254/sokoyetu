21d75b71ee12c41733eab38be12fa71ee7e5ddfb
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-03-01 14:47:33 +0300

    improved security

--- sokoyetu_project/local_settings.py
+++ sokoyetu_project/local_settings.py
@@ -1,23 +1,18 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oc^1694%m=5(p*nwz7_hh8ado-d7o9w+)#zj265_rwqml1kdk5'
+import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
+
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sokoyetu',
-        'USER': 'manu',
-        'PASSWORD': 'Manu@254#',
-        'HOST': 'localhost'
+        'NAME': os.getenv("DB_NAME", "sokoyetu"),
+        'USER': os.getenv("DB_USER", "manu"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "Manu@254#"),
+        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
-
-PAYPAL_CLIENT_ID = 'AWNABSsQoObcFHO66morybCLmz3IiQqU8TVxnwLOKb16Y1fav6bP6pRR6owEhqt1PFNhQO5QKt3ZbT9G'
-PAYPAL_SECRET = 'EPbogOQu9aABB643XAyEspBOgh6HMZSTd_vIUZty4ojYIVG6mW08HobPsKttgJdcD5mORoJSveeBz-WH'
